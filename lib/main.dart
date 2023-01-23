7692f81cafbd15787ba72689708c1e8c5a366b96
Le Nguyen Anh Khoi <khoi@tiny.fun>
2023-01-24 01:19:59 +0700

    Add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cash_management_project/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
-
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
