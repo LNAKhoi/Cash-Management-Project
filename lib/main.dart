0ed9c87815809a2d2ffb22e06b8f513aefa9f938
Le Nguyen Anh Khoi <khoi@tiny.fun>
2023-01-28 01:04:47 +0700

    Config firebase for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
+import 'package:cash_management_project/firebase_options.dart';
 import 'package:cash_management_project/screens/home/home_screen.dart';
 import 'package:cash_management_project/templates/custom_color.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cash_management_project/screens/login/login_screen.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
@@ -31,14 +32,13 @@ class MyApp extends StatelessWidget {
             }
             if (snapshot.hasError) {
               Fluttertoast.showToast(
-        msg: "Something went wrong, please try again!",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.white,
-        textColor: CustomColor.neutral100 ,
-        fontSize: 16.0
-    );
+                  msg: "Something went wrong, please try again!",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.white,
+                  textColor: CustomColor.neutral100,
+                  fontSize: 16.0);
             }
             if (snapshot.hasData) {
               return HomeScreen();
